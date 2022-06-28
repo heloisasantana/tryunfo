9f1dd1b812bdd925992ac838af121811203024b3
heloisasantana <heloisasantana.silva@hotmail.com>
2022-06-28 02:42:37 -0300

    Conclusão do Requisito 5

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
+    this.validateButton = this.validateButton.bind(this);
     this.state = {
       cardName: '',
       cardDescription: '',
@@ -15,12 +16,52 @@ class App extends React.Component {
       cardImage: '',
       cardRare: 'normal',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
   }
 
   handleChange(event) {
     const { name, type, value, checked } = event.target;
-    this.setState({ [name]: (type !== 'checkbox' ? value : checked) });
+    this.setState({
+      [name]: (type !== 'checkbox' ? value : checked) }, this.validateButton);
+  }
+
+  validateButton() {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+    let errorsForm = 0;
+    const convertedAttr1 = parseInt(cardAttr1, 10);
+    const convertedAttr2 = parseInt(cardAttr2, 10);
+    const convertedAttr3 = parseInt(cardAttr3, 10);
+    const sum = convertedAttr1 + convertedAttr2 + convertedAttr3;
+    const min = 0;
+    const max = 90;
+    const limitSum = 210;
+    if (sum > limitSum) { errorsForm += 1; }
+    if (convertedAttr1 < min
+     || convertedAttr2 < min
+     || convertedAttr3 < min
+     || convertedAttr1 > max
+     || convertedAttr2 > max
+     || convertedAttr3 > max) {
+      errorsForm += 1;
+    }
+    if (errorsForm === 0
+      && cardName !== ''
+      && cardDescription !== ''
+      && cardImage !== ''
+      && cardRare !== ''
+    ) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   }
 
   render() {
@@ -32,12 +73,14 @@ class App extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
         <Form
           onInputChange={ this.handleChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           cardName={ cardName }
